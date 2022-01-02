95f26450415c85655ffff1502b57523d4da68926
Eriton Xavier <ertao1@hotmail.com>
2022-01-02 09:00:11 -0300

    Implementado Board e Piece

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,5 +1,25 @@
 package boardgame;
 
 public class Piece {
+	
+	protected Position position;
+	private Board board;
+	
+	public Piece(Board board) {
+		this.board = board;
+		position = null;
+	}
+
+	protected Position getPosition() {
+		return position;
+	}
+
+		
+	
+	
+	
+	
+	
+	
 
 }
